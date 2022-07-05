8ca29b017b15c89b435889c150cebd73376c8276
Mariana <marianaufc@gmail.com>
2022-07-05 18:58:04 -0300

    feat(p01-socialmedia-feed): adds style to header component

--- p01-socialmedia-feed/src/components/Header.jsx
+++ p01-socialmedia-feed/src/components/Header.jsx
@@ -1,8 +1,11 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+
+import socialMediaLogo from '../assets/socialmedia-logo.svg';
 
 export function Header() {
     return(
         <header className={styles.header}>
+            <img src={socialMediaLogo} alt="Logotipo da SocialMedia" />
             <strong>Social Media Feed</strong>
         </header>
     )
