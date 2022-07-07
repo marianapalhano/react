c535076de8ae0d64d6c9e9430ff60029744a7d2f
Mariana <marianaufc@gmail.com>
2022-07-07 10:30:11 -0300

    feat(p01-socialmedia-feed): adds sidebar styles

--- p01-socialmedia-feed/src/components/Sidebar.jsx
+++ p01-socialmedia-feed/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 import cover from '../assets/cover.jpg';
 
@@ -7,11 +8,15 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img src={cover} className={styles.cover} />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/marianapalhano.png" />
                 <strong>Mariana Palhano</strong>
                 <span>Dungeon Master</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
